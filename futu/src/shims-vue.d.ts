c3acc24b6f05e07f87f62d54a565df29e4578e3f
Khant Loon Thu <142862466+KhantLoonThu@users.noreply.github.com>
2024-06-30 13:44:05 +0630

    Remove futu directory from repository

--- futu/src/shims-vue.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-declare module "*.vue" {
-  import { ComponentOptions } from "vue";
-  const component: ComponentOptions;
-  export default component;
-}
--- futu/src/types/state.ts
+++
@@ -1,9 +0,0 @@
-import { BirthDate, Customer } from "./customer";
-
-// types/state.ts
-export default interface State {
-  theme: string;
-  customer: Customer;
-  birthDate: BirthDate;
-  serverErrors: string[];
-}
